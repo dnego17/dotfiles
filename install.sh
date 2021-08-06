662306ccbb600c2742af5c4057a12f2219dd6345
yh <you@example.com>
2021-08-06 13:21:39 +0900

    tmux conf

--- install.sh
+++ install.sh
@@ -1,7 +1,14 @@
+chsh -s $(which zsh)
+apt-get update && apt-get install -y make
 mkdir ~/.zinit
 git clone --depth 1 https://github.com/zdharma/zinit.git ~/.zinit/bin
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install --all
-curl -sS https://webinstall.dev/zoxide | bash
+apt-get update && apt-get install -y tmux
+mkdir ~/.tmux
+wget -O $HOME/.tmux/iceberg.tmux.conf \
+https://raw.githubusercontent.com/gkeep/iceberg-dark/master/.tmux/iceberg.tmux.conf
+
 ln -snfv "$(pwd)/.zshrc" "$HOME/.zshrc"
+ln -snfv "$(pwd)/.tmux.conf" "$HOME/.tmux.conf"
 echo "Success"
